5740d72595117a253f3930c5df6ab5bf5c64fb46
kittyspet <109090101+kittyspet@users.noreply.github.com>
2024-09-30 19:18:25 +0300

    lab 6

---
+++ lab_06/database/report.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <optional>
+
+#include <Poco/JSON/Object.h>
+
+namespace database
+{
+    class Report{
+        private:
+            long _id;
+            std::string _report_name;
+            std::string _report_text;
+            long _author_id;
+            std::string _date_report;
+            struct Comment {
+                std::string comment_text;
+                long commentator_id;
+                std::string date_comment;
+                const std::string to_string() const;
+            };
+            Comment _comment;
+
+
+        public:
+
+            static Report fromJSON(const std::string & str);
+
+            long               get_id() const;
+            const std::string &get_report_name() const;
+            const std::string &get_report_text() const;
+            long               get_author_id() const;
+            const std::string &get_date_report() const;
+            const std::string  get_comment_as_string() const;
+
+            long&        id();
+            std::string& report_name();
+            std::string& report_text();
+            long&        author_id();
+            std::string& date_report();
+            Comment&     comment();
+
+            static std::optional<Report> read_by_id(long id);
+            static std::vector<Report> read_by_author_id(long author_id);
+            void add();
+            void update();
+            Poco::JSON::Object::Ptr toJSON() const;
+
+    };
+}
---
+++ lab_06/start_gateway.sh
@@ -0,0 +1,16 @@
+export DB_HOST=lab_06_postgres
+export DB_PORT=5432
+export DB_LOGIN=root
+export DB_PASSWORD=root
+export DB_DATABASE=archdb
+export MONGO_HOST=lab06_mongo
+export MONGO_PORT=27017
+export MONGO_DATABASE=arch
+export JWT_KEY=0123456789ABCDEF0123456789ABCDEF
+export CACHE=lab_06_cache:6379
+
+export USER_ADDRESS=http://lab_06_user:8080
+export CONF_ADDRESS=http://lab_06_conf:8080
+export REPORT_ADDRESS=http://lab_06_report:8080
+
+/app/build/lab_06_gateway
